30a8bcd254f9084f5d503d4a5f24832861b710c8
kermtuncr19 <kerimtuncer190748@gmail.com>
2024-04-22 19:11:51 +0300

    commit

---
+++ src/test/java/org/example/CalculatorTest.java
@@ -0,0 +1,50 @@
+// B211202040 - Kerim TUNÇER
+// SWE 202 SOFTWARE VERIFICATION AND VALIDATION - HOMEWORK1
+//
+
+package org.example;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "10, 2, 5",
+            "10, 4, 2.5",
+            "12.5, 2.5, 5",
+            "10, 2.5, 4",
+            "12.5, 5, 2.5"
+    })
+
+    void testDivision(float dividend, float divisor, float expectedResult) {
+        float result = Calculator.divide(dividend,divisor);
+        assertEquals(expectedResult, result);
+    }
+
+    @Test
+     void testDivision6() {
+        Exception exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> Calculator.divide(12.5f, 0),
+                "IllegalArgumentException expected."
+        );
+
+        // Optional. To check whether the error messages match.
+        assertEquals("Illegal Argument Exception.", exception.getMessage());
+    }
+}
